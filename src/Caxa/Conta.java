f56a321787a806082711bcfe56b52665442eeaac
RobertoMoniz <robertomoniz_3@hotmail.com>
2022-03-03 23:09:01 -0300

    Initial commit

---
+++ src/Caxa/Conta.java
@@ -0,0 +1,12 @@
+package Caixa;
+
+import Caxa.Caixa;
+
+public class Conta extends Caixa {
+    public Conta(String nome, int conta, double inicial) {
+        super();
+    }
+
+    public void inicial() {
+    }
+}
